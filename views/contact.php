a0634adca0194be27242e52bff8d10f54398b9da
Fernando <36939637+ferdrun@users.noreply.github.com>
2024-04-18 20:48:01 -0300

    Add files via upload

---
+++ views/contact.php
@@ -0,0 +1,8 @@
+<div class="container">
+				<div class="row">
+					 
+                    <ul>
+					<li class="head_help">WHATSAPP: (11) 9999-9999</li>
+					<li class="head_email">Email: contato@<span>loja2.com.br</span></li>
+				</ul>
+                </div></div>
